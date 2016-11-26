6e41fff465c0317386650ab6eed7a8d834811ae8
schiller <schillerbr@gmail.com>
2016-11-25 22:18:09 -0200

    Chose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -32,10 +32,18 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
+from time import time
 
 clf = AdaBoostClassifier()
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
 pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
 acc = accuracy_score(pred, labels_test)
 print acc
 
