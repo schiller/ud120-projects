a7e55d436da3443fa9e994e2b4b8b63fd471657c
Luiz Schiller <schillerbr@gmail.com>
2016-12-01 12:27:32 -0200

    text_learning

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,15 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+        stemmer = SnowballStemmer("english")
+        split_words = text_string.split()
+        split_words = [stemmer.stem(word) for word in split_words]
+        words = " ".join(split_words)
 
     return words
 
